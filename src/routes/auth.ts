e6c2ada011e39b9599a3cd77d8f28e37bc14dba7
albertobasaglia <alberto.basaglia17@gmail.com>
2019-11-12 16:35:17 +0100

    add login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,6 +5,7 @@ import { Request, Response } from "express-serve-static-core";
 import { getManager } from "typeorm";
 import { User } from "../entity/User";
 import * as bcrypt from "bcrypt";
+import * as jsonwebtoken from "jsonwebtoken";
 export const authRouter = Express.Router();
 authRouter.post('/register', [
     check('username').isLength({ min: 5, max: 35 }),
@@ -31,6 +32,38 @@ authRouter.post('/register', [
             })
         } else {
             res.status(409).send({msg: 'user already registered!'});
+        }
+    }
+});
+authRouter.post('/login', [
+    check('email').isEmail(),
+    check('password').isLength({ min: 8, max: 35 }),
+], async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(422).send({ errors });
+    } else {
+        const userRepo = getManager().getRepository(User);
+        const user = await userRepo.findOne({ where: { email: req.body.email } });
+        if(!user) {
+            res.status(401).send({msg: 'credenziali errate!'});            
+        } else {
+            bcrypt.compare(req.body.password,user.passwordHash,(err,same) => {
+                if(err) {
+                    throw err;
+                }
+                if(same) {
+                    jsonwebtoken.sign({id: user.id,email: user.email},"secret",{expiresIn: "2 days"},(err,encoded) => {
+                        if(err) {
+                            throw err;
+                        } else {
+                            res.status(200).send(encoded);
+                        }
+                    })
+                } else {
+                    res.status(401).send({msg: 'credenziali errate!'});  
+                }
+            })
         }
     }
 });
