dbcd18443dff9a426343ed2fbb88c5405ad060b4
albertobasaglia <alberto.basaglia17@gmail.com>
2019-11-13 12:54:14 +0100

    check if user is in chat

--- src/routes/chat/chat.ts
+++ src/routes/chat/chat.ts
@@ -24,55 +24,87 @@ chatRouter.post('/create', [check('name').isLength({max: 25, min: 5})] ,async (r
 });
 chatRouter.post('/:chatId/sendMessage',[check('text').isLength({min:1,max:1024})],async (req: Express.Request,res: Express.Response) => {
     const chatRepository = getManager().getRepository(Chat);
+    const chatWithUsers = await chatRepository.findOne({where: {id: req.params.chatId},relations: ["users"]});
     const chat = await chatRepository.findOne({where: {id: req.params.chatId},relations: ["messages"]});
     const user = await getManager().getRepository(User).findOne({where: {id: req.userId}});
-    const message = new Message();
-    message.writtenBy = user;
-    message.text = req.body.text;
-    message.sentTime = new Date();
-    if(!chat.messages) {
-        chat.messages = [];
-    }
-    chat.messages.push(message);
-    const savedChat = await chatRepository.save(chat);
-    res.status(200).send({id: message.id});
+    if(chatWithUsers.users.indexOf(user) === -1)  {
+        res.status(403).send({msg: 'Non hai i permessi!'});
+    } else {
+        const message = new Message();
+        message.writtenBy = user;
+        message.text = req.body.text;
+        message.sentTime = new Date();
+        if(!chat.messages) {
+            chat.messages = [];
+        }
+        chat.messages.push(message);
+        const savedChat = await chatRepository.save(chat);
+        res.status(200).send({id: message.id});
+    }    
 });
 chatRouter.get('/:chatId/getAllMessages',async (req,res) => {
     const messageRepository = getManager().getRepository(Message);
-    const messages = await messageRepository.createQueryBuilder('message')
-        .innerJoin('message.chat','chat')
-        .where('chat.id = :chatId',{chatId: req.params.chatId})
-        .orderBy('message.sentTime','DESC')
-        .getMany();
-    res.send(messages);
+    const chatRepository = getManager().getRepository(Chat);
+    const chatWithUsers = await chatRepository.findOne({where: {id: req.params.chatId},relations: ["users"]});
+    const user = await getManager().getRepository(User).findOne({where: {id: req.userId}});
+    if(chatWithUsers.users.indexOf(user) === -1)  {
+        res.status(403).send({msg: 'Non hai i permessi!'});
+    } else {
+        const messages = await messageRepository.createQueryBuilder('message')
+            .innerJoin('message.chat','chat')
+            .where('chat.id = :chatId',{chatId: req.params.chatId})
+            .orderBy('message.sentTime','DESC')
+            .getMany();
+        res.send(messages);
+    }
 });
 chatRouter.get('/:chatId/getLastMessages', [check('count').isNumeric()], async (req,res) => {
     const messageRepository = getManager().getRepository(Message);
-    const messages = await messageRepository.createQueryBuilder('message')
-        .innerJoin('message.chat','chat')
-        .where('chat.id = :chatId',{chatId: req.params.chatId})
-        .orderBy('message.sentTime','DESC')
-        .limit(req.query.count)
-        .getMany();
-    res.send(messages);
+    const chatRepository = getManager().getRepository(Chat);
+    const chatWithUsers = await chatRepository.findOne({where: {id: req.params.chatId},relations: ["users"]});
+    const user = await getManager().getRepository(User).findOne({where: {id: req.userId}});
+    if(chatWithUsers.users.indexOf(user) === -1)  {
+        res.status(403).send({msg: 'Non hai i permessi!'});
+    } else {
+        const messages = await messageRepository.createQueryBuilder('message')
+            .innerJoin('message.chat','chat')
+            .where('chat.id = :chatId',{chatId: req.params.chatId})
+            .orderBy('message.sentTime','DESC')
+            .limit(req.query.count)
+            .getMany();
+        res.send(messages);
+    }
 });
 chatRouter.get('/:chatId/getMessagesSince',[check('datetime').isAlphanumeric()], async (req,res) => {
     const messageRepository = getManager().getRepository(Message);
-    const messages = await messageRepository.createQueryBuilder('message')
-        .innerJoin('message.chat','chat')
-        .where('chat.id = :chatId',{chatId: req.params.chatId})
-        .orderBy('message.sentTime','DESC')
-        .where('message.sentTime >= :datetime',{datetime: new Date(req.query.datetime)})
-        .getMany();
-    res.send(messages);
+    const chatRepository = getManager().getRepository(Chat);
+    const chatWithUsers = await chatRepository.findOne({where: {id: req.params.chatId},relations: ["users"]});
+    const user = await getManager().getRepository(User).findOne({where: {id: req.userId}});
+    if(chatWithUsers.users.indexOf(user) === -1)  {
+        res.status(403).send({msg: 'Non hai i permessi!'});
+    } else {
+        const messages = await messageRepository.createQueryBuilder('message')
+            .innerJoin('message.chat','chat')
+            .where('chat.id = :chatId',{chatId: req.params.chatId})
+            .orderBy('message.sentTime','DESC')
+            .where('message.sentTime >= :datetime',{datetime: new Date(req.query.datetime)})
+            .getMany();
+        res.send(messages);
+    }
 });
 chatRouter.get('/:chatId/getMessagesAfter', async (req,res) => {
-    const messageRepository = getManager().getRepository(Message);
-    const messages = await messageRepository.createQueryBuilder('message')
-        .innerJoin('message.chat','chat')
-        .where('chat.id = :chatId',{chatId: req.params.chatId})
-        .orderBy('message.sentTime','DESC')
-        .where('message.id > :id',{id: req.query.id})
-        .getMany();
-    res.send(messages);
+    const messageRepository = getManager().getRepository(Message);const chatRepository = getManager().getRepository(Chat);
+    const chatWithUsers = await chatRepository.findOne({where: {id: req.params.chatId},relations: ["users"]});
+    const user = await getManager().getRepository(User).findOne({where: {id: req.userId}});
+    if(chatWithUsers.users.indexOf(user) === -1)  {
+        res.status(403).send({msg: 'Non hai i permessi!'});
+    } else {
+        const messages = await messageRepository.createQueryBuilder('message')
+            .innerJoin('message.chat','chat')
+            .where('chat.id = :chatId',{chatId: req.params.chatId})
+            .orderBy('message.sentTime','DESC')
+            .where('message.id > :id',{id: req.query.id})
+            .getMany();
+        res.send(messages);
+    }
 })
